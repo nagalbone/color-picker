346863a53914f9ae55e4edc5a87e043cfd798b3c
Arvind <arvindnagalbone@gmail.com>
2024-03-05 09:37:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,61 +1,21 @@
 import { useState } from "react";
-import "./App.css";
 
-function App() {
-  const [addColor, setAddColor] = useState("#CCCCFF");
-  const colorsArray = [
-    "#CD5C5C",
-    "#6495ED",
-    "#40E0D0",
-    "#9FE2BF",
-    "#DE3163",
-    "#FF7F50",
-    "#FFBF00",
-    "#DFFF00",
-    "#FFA07A",
-    "#F08080",
-  ];
+const App = (props) => {
+  const [count, setCount] = useState(0);
   return (
-    <div className="cursor-pointer">
-      <ColorHeader headerColor={colorsArray} setAddColor={setAddColor} />
-      {colorsArray.map(() => {
-        return (
-          <div className="flex">
-            {colorsArray.map(() => (
-              <SubColor addColor={addColor} />
-            ))}
-          </div>
-        );
-      })}
+    <div>
+      <h1>Count: {count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Count
+      </button>
+      <h1>Name: {props.name}</h1>
+      <h1>Age: {props.age}</h1>
     </div>
   );
-}
-
-function ColorHeader({ headerColor, setAddColor }) {
-  return (
-    <div className="flex">
-      {headerColor.map((color) => {
-        return (
-          <div
-            onClick={() => setAddColor(color)}
-            className="h-8 w-8 m-1"
-            style={{ backgroundColor: color }}
-          ></div>
-        );
-      })}
-    </div>
-  );
-}
-
-function SubColor({ addColor }) {
-  const [color, setColor] = useState("#CCCCFF");
-  return (
-    <div
-      onClick={() => setColor(addColor)}
-      className="h-8 w-8 m-1"
-      style={{ backgroundColor: color }}
-    ></div>
-  );
-}
+};
 
 export default App;
